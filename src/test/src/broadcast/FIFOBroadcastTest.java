b988ab0400d51af5bae0871898b51bc1d65b11ef
11721387 <e11721t387@student.tuwien.ac.at>
2018-10-16 10:12:40 +0200

    FIFO test not done

---
+++ src/test/src/broadcast/FIFOBroadcastTest.java
@@ -0,0 +1,117 @@
+package src.broadcast;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import src.data.message.BroadcastMessage;
+import src.data.message.Message;
+import src.data.message.SimpleMessage;
+import src.exception.BadIPException;
+import src.exception.UnreadableFileException;
+import src.observer.broadcast.FIFOBroadcastObserver;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FIFOBroadcastTest {
+
+    private static final int SENDER_PORT = 11001;
+    private static final int SENDER_ID = 1;
+    private static final int[] RECEIVER_PORTS = {11002, 11003, 11004, 11005};
+
+    private static final String MSG_TEXT = "Hello World";
+    private static final Message SIMPLE_MSG = new SimpleMessage(MSG_TEXT);
+    private static final int MSG_SEQ_NUM = 2;
+    private static final BroadcastMessage BROADCAST_MESSAGE =
+            new BroadcastMessage(SIMPLE_MSG, MSG_SEQ_NUM, SENDER_ID);
+
+    private class TestObserver implements FIFOBroadcastObserver {
+
+        private boolean delivered = false;
+        private Message message;
+        private int senderID;
+
+        @Override
+        public void deliverFIFOB(Message msg, int senderID) {
+            if (!delivered) {
+                this.delivered = true;
+                this.message = msg;
+                this.senderID = senderID;
+            }
+        }
+
+        Message getMessage() { return message; }
+        int getSenderID() { return senderID; }
+        boolean isDelivered() { return delivered; }
+    }
+
+
+
+    @Test
+    void simpleMessageWorks() {
+
+        FIFOBroadcast sender = null;
+
+        List<FIFOBroadcast> receivers = new ArrayList<>();
+
+        String testIP = "127.0.0.1";
+
+
+        try {
+            sender = new FIFOBroadcast(testIP, SENDER_PORT);
+
+            for(int port : RECEIVER_PORTS) {
+                receivers.add(new FIFOBroadcast(testIP,port));
+            }
+        } catch (SocketException | BadIPException | UnreadableFileException | UnknownHostException e) {
+            Assertions.fail("Exception thrown: " + e.getMessage());
+        }
+
+        List<TestObserver> receiverObservers = new ArrayList<>();
+
+        for(FIFOBroadcast urb : receivers) {
+            TestObserver observer = new TestObserver();
+            receiverObservers.add(observer);
+            urb.registerObserver(observer);
+        }
+
+        try {
+            sender.broadcast(BROADCAST_MESSAGE);
+        } catch (BadIPException | IOException | UnreadableFileException e) {
+            Assertions.fail(e.getMessage());
+        }
+
+        //Wait for delivery
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            Assertions.fail(e.getMessage());
+        }
+
+
+        for(TestObserver obs : receiverObservers) {
+            Assertions.assertTrue(obs.isDelivered());
+
+            Assertions.assertEquals(SENDER_ID, obs.getSenderID());
+
+            BroadcastMessage m = (BroadcastMessage) obs.getMessage();
+
+            Assertions.assertEquals(SIMPLE_MSG, m.getMessage());
+            Assertions.assertEquals(MSG_SEQ_NUM, m.getMessageSequenceNumber());
+            Assertions.assertEquals(SENDER_ID, m.getOriginalSenderID());
+        }
+
+        try {
+            sender.shutdown();
+            for(FIFOBroadcast fifoBroadcast : receivers) {
+                fifoBroadcast.shutdown();
+            }
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+
+    }
+}
